e1f3968431a3aaede2bbc54f54bbf4987a271c4a
Prithi <pprithip@sfu.ca>
2019-12-18 01:56:00 -0800

    Added Slack rate-limit slack support

--- stembot/basic.py
+++ stembot/basic.py
@@ -8,14 +8,7 @@ from typing import Dict, List
 from slack import WebClient as slack_client
 
 
-def measure_time(func):
-    def wrapper(*args,**kwargs):
-        start = time.time()
-        returnval = func(*args,**kwargs)
-        end = time.time() 
-        print("Time taken by {} is {}".format(func.__name__,end-start))
-        return returnval
-    return wrapper
+
     
 ## DEFAULT CONFIGURATIONS DEFINITIONS
 
@@ -23,7 +16,9 @@ MENTOR_CHANNEL_NAME="mentors"
 MY_STEMBOT_TOKEN=os.getenv("STEMBOT_TOKEN")
 CHANNEL_NUM = 500
 NUM_REQUESTS = 0
-RATE_LIMIT_TIME_WAIT = 60
+RATE_LIMIT_TIME_WAIT = 120
+MAX_CHANNEL_NUM=100
+NUM_FUNC_CALLS=0
 
 client = slack_client(token=MY_STEMBOT_TOKEN)
 
@@ -33,8 +28,28 @@ PUBLIC_CHANNELS=client.conversations_list(types="public_channel",limit=CHANNEL_N
 PRIVATE_CHANNELS=client.conversations_list(types="private_channel",limit=CHANNEL_NUM)["channels"]
 NUM_REQUESTS+=2
 
+## WRAPPER FUNCTIONS 
+def measure_time(func):
+    def wrapper(*args,**kwargs):
+        start = time.time()
+        returnval = func(*args,**kwargs)
+        end = time.time() 
+        print("Time taken by {} is {}".format(func.__name__,end-start))
+        return returnval
+    return wrapper
+
+def recored_function_calls(func) : 
+    def wrapper(*args,**kwargs) : 
+        global NUM_FUNC_CALLS 
+        NUM_FUNC_CALLS+=1
+        returnval = func(*args,**kwargs)
+        return returnval
+    return wrapper
+
+
 client = slack_client(token=MY_STEMBOT_TOKEN)
 
+@recored_function_calls
 def post_message_to_slack(channel_name,text_to_post) : 
     response = client.chat_postMessage(
             channel=channel_name,
@@ -44,6 +59,7 @@ def post_message_to_slack(channel_name,text_to_post) :
     assert response["message"]["text"] == "Hello world!"
 
 ## test type checking
+@recored_function_calls
 def get_channel_id(channel_name : str,is_public : bool) -> int:
     
     #t1 = time.time()
@@ -70,16 +86,23 @@ def get_channel_id(channel_name : str,is_public : bool) -> int:
         return current_channel_id[0]
     
 ## test type checking
+@recored_function_calls
 def get_channel_members_ids(channel_name : str , is_public : bool ) -> List[int] :
     global NUM_REQUESTS
     
+    t1 = time.time()
     channel_id = get_channel_id(channel_name,is_public)
-    channel_members = client.conversations_members(channel=channel_id,limit=100)["members"]
+    t2 = time.time()
+    channel_members = client.conversations_members(channel=channel_id,limit=MAX_CHANNEL_NUM)["members"]
+    t3 = time.time()
+
+    print("t1 = {}, t2 = {}".format(t2-t1,t3-t2))
     NUM_REQUESTS+=1
     #print(channel_members)
     return channel_members
     
 ## test type checking
+@recored_function_calls
 def get_member_info(member_id : str ) -> Dict[str,str] : 
     global NUM_REQUESTS
     
@@ -109,6 +132,7 @@ def get_channel_info(channel_name : str,is_public : bool) -> Dict[str,str] :
     return {'creator':chan_creator,'team_id':team_id,'members':channel_member_ids}
 
 ## add type checking and test type checking
+@recored_function_calls
 def channel_message_analysis(channel_name,is_public) : 
     
     global NUM_REQUESTS
@@ -166,14 +190,22 @@ def channel_message_analysis(channel_name,is_public) :
     # Rate Limit Exceeded : 
     except Exception as e : 
         print("--> channel_message_analysis : Error : {} . Exiting..".format(e))
-        if "ratelimited" in e : 
-            print('--> Total requests made = {}'.format(NUM_REQUESTS))
-        return {}
+        x = e.args
+        #print("Exception args = {}".format(x))
+        for err in e.args : 
+            if 'user' in err : 
+                print('--> Total requests made = {}'.format(NUM_REQUESTS))
+                return {'error':'user'}
+            if 'ratelimited' in err : 
+                ## 10s sleep before making anyother request.
+                ## Slack Api rate limit info can be found here : https://api.slack.com/docs/rate-limits
+                print('--> Total requests made = {}'.format(NUM_REQUESTS))
+                return {'error':'rate_limited'}
+        return {'error':e.args[0]}
 
-@measure_time
+@recored_function_calls
 def generate_num_messages_all(to_pickle=False,to_csv=True) : 
     
-    all_public_channels =  [i["name"] for i in PUBLIC_CHANNELS]
     all_private_channels = [i["name"] for i in PRIVATE_CHANNELS] 
 
     all_data = {}
@@ -181,18 +213,34 @@ def generate_num_messages_all(to_pickle=False,to_csv=True) :
     
     for c in all_private_channels : 
         output = channel_message_analysis(c,is_public=False)
-        if output != {} : 
+        
+        if 'error' not in output : 
             all_data = {**all_data,**output}
         else:
             missed_channels.append((c,False))
+        
+    all_public_channels =  [i["name"] for i in PUBLIC_CHANNELS]
     
     for c in all_public_channels : 
         output = channel_message_analysis(c,is_public=True)
-        if output!={} : 
+        
+        if 'error' not in output : 
             all_data = {**all_data,**output}
         else:
             missed_channels.append((c,True))
 
+
+    print("--> Waiting ({}s) and starting Missing Channels processing ".format(RATE_LIMIT_TIME_WAIT))
+    
+    time.sleep(RATE_LIMIT_TIME_WAIT)
+    
+    for c in missed_channels : 
+        chan_name = c[0]
+        is_public = c[1]
+        output = channel_message_analysis(chan_name,is_public=is_public)
+        if output == {}:
+            print("{} unprocessed ".format(chan_name))
+        all_data={**all_data,**output}
 
     if to_pickle : 
         pickle.dump(all_data,open("generate_num_messages_all.p","wb"))
@@ -201,6 +249,8 @@ def generate_num_messages_all(to_pickle=False,to_csv=True) :
     if to_csv : 
         df = pd.DataFrame({'name':list(all_data.keys()),'number_of_messages':list(all_data.values())})
         df.to_csv('general_num_messages_all.csv')
+
+    
     return all_data 
 
 
